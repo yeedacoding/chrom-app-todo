5038d080435f1abeed1219b5ab04f3cb4dbc6e66
yeedacoding <taeheun0325@naver.com>
2021-07-01 22:25:59 +0900

    Created random quotes & authors

---
+++ js/quotes.js
@@ -0,0 +1,57 @@
+const quotes = [
+    {
+      quote: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney",
+    },
+    {
+      quote: "Life is what happens when you're busy making other plans.",
+      author: "John Lennon",
+    },
+    {
+      quote:
+        "The world is a book and those who do not travel read only one page.",
+      author: "Saint Augustine",
+    },
+    {
+      quote: "Life is either a daring adventure or nothing at all.",
+      author: "Helen Keller",
+    },
+    {
+      quote: "To Travel is to Live",
+      author: "Hans Christian Andersen",
+    },
+    {
+      quote: "Only a life lived for others is a life worthwhile.",
+      author: "Albert Einstein",
+    },
+    {
+      quote: "You only live once, but if you do it right, once is enough.",
+      author: "Mae West",
+    },
+    {
+      quote: "Never go on trips with anyone you do ntot love.",
+      author: "Hemmingway",
+    },
+    {
+      quote: "We wander for distraction, but we travel for fulfilment.",
+      author: "Hilaire Belloc",
+    },
+    {
+      quote: "Travel expands the mind and fills the gap.",
+      author: "Sheda Savage",
+    },
+  ];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+//quotes array를 인덱싱 = quotes[]  --> 대괄호 안의 코드는 결국 숫자이다.
+//Math.random()  --> 0부터 1사이 숫자를 랜덤하게 뽑는다(소수점 포함)
+//Math.floor()  ---> 내림
+//Math.ceil()   ---> 올림
+//Math.round()  ---> 반올림
+
+quote.innerText = `"${todayQuote.quote}"`;
+author.innerText = todayQuote.author;
